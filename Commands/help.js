649dae9eedce4cf5e098765b8f57b01def3ce29f
st2394580-lab <st2394580@gmail.com>
2026-08-28 07:58:52 +0530

    Implement help command for DEMON-BOT
    Added help command for DEMON-BOT with usage examples.

--- Commands/help.js
+++ Commands/help.js
@@ -1,1 +1,48 @@
+const config = require("../config");
 
+module.exports = async function help({
+  sock,
+  jid,
+  message
+}) {
+  const text = `
+🆘 *DEMON-BOT HELP*
+
+Command format:
+
+${config.prefix}command
+
+Example:
+
+${config.prefix}ping
+${config.prefix}menu
+${config.prefix}alive
+${config.prefix}info
+
+For group commands:
+
+${config.prefix}groupinfo
+${config.prefix}setname New Name
+
+Use ${config.prefix}menu to see all commands.
+`;
+
+  await sock.sendMessage(
+    jid,
+    { text },
+    { quoted: message }
+  );
+};
+
+module.exports.telegram = async function telegramHelp(ctx) {
+  await ctx.reply(`
+🆘 DEMON-BOT HELP
+
+/ping
+/menu
+/alive
+/info
+
+Use /menu for the complete command list.
+`);
+};
