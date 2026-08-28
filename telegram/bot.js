c8659d9c1d18ca2ddec72fad4f7b52fbdff300bf
st2394580-lab <st2394580@gmail.com>
2026-08-28 07:54:43 +0530

    Implement Telegram bot initialization and handlers

--- telegram/bot.js
+++ telegram/bot.js
@@ -1,1 +1,36 @@
+const { Telegraf } = require("telegraf");
 
+const config = require("../config");
+const handler = require("./handler");
+const logger = require("../utils/logger");
+
+let bot = null;
+
+async function startTelegram() {
+  if (!config.telegramToken) {
+    logger.warn("TELEGRAM_TOKEN not configured. Telegram disabled.");
+    return null;
+  }
+
+  bot = new Telegraf(config.telegramToken);
+
+  handler.registerTelegramHandlers(bot);
+
+  await bot.launch();
+
+  logger.success("Telegram bot started.");
+
+  process.once("SIGINT", () => bot.stop("SIGINT"));
+  process.once("SIGTERM", () => bot.stop("SIGTERM"));
+
+  return bot;
+}
+
+function getTelegramBot() {
+  return bot;
+}
+
+module.exports = {
+  startTelegram,
+  getTelegramBot
+};
