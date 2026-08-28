eaef3c9a887b06f0f23a30496283530396944b35
st2394580-lab <st2394580@gmail.com>
2026-08-28 07:59:32 +0530

    Implement alive command for bot status notification

--- Commands/alive.js
+++ Commands/alive.js
@@ -1,1 +1,21 @@
+const config = require("../config");
 
+module.exports = async function alive({
+  sock,
+  jid,
+  message
+}) {
+  await sock.sendMessage(
+    jid,
+    {
+      text: `👹 *${config.botName}*\n\n✅ Bot is alive!\n⚡ Status: Online`
+    },
+    { quoted: message }
+  );
+};
+
+module.exports.telegram = async function telegramAlive(ctx) {
+  await ctx.reply(
+    `👹 ${config.botName}\n\n✅ Bot is alive!\n⚡ Status: Online`
+  );
+};
