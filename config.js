b8cafd8d4a18ee91865b9e8e4606e58efd8f3ec5
st2394580-lab <st2394580@gmail.com>
2026-08-28 07:51:45 +0530

    Add environment variable configuration to config.js

--- config.js
+++ config.js
@@ -1,1 +1,19 @@
 
+require("dotenv").config();
+
+module.exports = {
+  botName: process.env.BOT_NAME || "DEMON-BOT",
+  prefix: process.env.PREFIX || ".",
+
+  ownerNumber: process.env.OWNER_NUMBER || "",
+
+  telegramToken: process.env.TELEGRAM_TOKEN || "",
+
+  sessionDir: process.env.SESSION_DIR || "./session",
+
+  autoReply: process.env.AUTO_REPLY !== "false",
+
+  welcome: process.env.WELCOME !== "false",
+
+  cooldownMs: Number(process.env.COOLDOWN_MS || 3000)
+};
